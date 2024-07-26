34d2fd81198cc5a5529c15a08650e5a92ecd849c
julianojbpg <juliano-jbpg@hotmail.com>
2024-07-26 20:35:51 -0300

    classBanco

---
+++ src/@types/index.ts
@@ -0,0 +1,9 @@
+
+
+export interface tipoDB{
+    database: string | undefined
+    username: string | undefined
+    password: string | undefined
+    host: string | undefined
+    dialect: string | undefined
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express'
-
+import {TabelaUsuarios} from './database/model/tabelas'
 const app = express()
 
+TabelaUsuarios()
 app.listen(8080,()=>{
     console.log(`servidor rodando na porta 8080`)
 })
