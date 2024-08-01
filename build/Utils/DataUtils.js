a522d4fd0ee259a64c6c0884e6d9edebbc05d8ce
hugoart05 <comercialhugo.s@gmail.com>
2024-08-01 00:42:14 -0300

    delete build

--- build/Utils/DataUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetDataAtual = GetDataAtual;
-function GetDataAtual() {
-    var date = Date.now();
-    return new Date(date);
-}
